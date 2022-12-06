2f96bc5ef33edeecdd646d1437559f48cf2ef56f
mehmet cantemir <mehmetcantemir81@gmail.com>
2022-12-06 17:01:55 +0300

    While Loop Kodluyoruz

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1 den başlayarak console dan girilens ayıya kadar ortalama hesaplayıp console yazdıran program
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sayac= 1;
+            int toplam = 0;
+            while (sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine("Toplam sayının ortalaması = " + (toplam / sayi));
+        }
+    }
+}
